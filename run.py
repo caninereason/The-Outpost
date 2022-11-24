412c9668b91a35be3c5f0227bf51a12e425c2227
caninereason <theciaranneeson@hotmail.com>
2022-11-24 22:48:15 +0000

    add dprint function

--- run.py
+++ run.py
@@ -1,13 +1,31 @@
+import time
+import sys
+
+def dprint(s):
+    for c in s:
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(0.02)
 begin = ["play","p","y","yes","start","s"]
 left = ["left","l","w","west"]
 right =["right","r","east","e"]
 straight = ["forward","f","north","n"]
 back = ["back","b","south","s"]
-name = input("What is your name ")
-print(f'Greetings {name}! welcome to the outpost')
-start = input ('Do you wish to start the game or not ')
+dprint("What is your name ")
+name = input()
+dprint(f'Greetings {name}! welcome to the outpost')
+dprint ('Do you wish to start the game or not ')
+start = input()
 if start in begin:
-    print('let us begin')
+    dprint('let us begin')
+    dprint('.')
+    time.sleep(1)
+    dprint('.')
+    time.sleep(1)
+    dprint('.')
+    time.sleep(1)
+    dprint
 else:
-    print('well I guess you better get on with your incredible existence then, Human..')
+    dprint('well I guess you better get on with your incredible existence then, Human..')
     quit()
+ 
