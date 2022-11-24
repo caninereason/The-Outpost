df014ce638d461e0ef5e435c5823a0cab32bd10c
caninereason <theciaranneeson@hotmail.com>
2022-11-24 22:45:59 +0000

    add direction arrays

--- run.py
+++ run.py
@@ -1,11 +1,13 @@
-
-
-
+begin = ["play","p","y","yes","start","s"]
+left = ["left","l","w","west"]
+right =["right","r","east","e"]
+straight = ["forward","f","north","n"]
+back = ["back","b","south","s"]
 name = input("What is your name ")
 print(f'Greetings {name}! welcome to the outpost')
 start = input ('Do you wish to start the game or not ')
-if start == 'play'or'p'or'y'or'yes'or'start'or's':
+if start in begin:
     print('let us begin')
 else:
-    print('well I guess you better get on with your incredible existence then, Human')
+    print('well I guess you better get on with your incredible existence then, Human..')
     quit()
