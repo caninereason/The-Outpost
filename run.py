e874f5bdd5ceb5be05c329f62a81fffc9476a07d
caninereason <theciaranneeson@hotmail.com>
2022-11-24 22:40:38 +0000

    initial commit

--- run.py
+++ run.py
@@ -1,3 +1,8 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+name = input("What is your name ")
+print(f'Greetings {name}! welcome to the outpost')
+start = input ('Do you wish to start the game or not ')
+if start == 'play||p||y||yes||start||s':
+    print('let us begin')
+else:
+    print('well I guess you better get on with your amazing existence then')
+    quit()
