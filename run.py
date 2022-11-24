340f46e3a7e40b3e194201f94cdd85508eb8a377
caninereason <theciaranneeson@hotmail.com>
2022-11-24 23:22:16 +0000

    add initial text

--- run.py
+++ run.py
@@ -14,17 +14,20 @@ back = ["back","b","south","s"]
 dprint("What is your name ")
 name = input()
 dprint(f'Greetings {name}! welcome to the outpost')
-dprint ('Do you wish to start the game or not ')
+dprint('.')
+time.sleep(1)
+dprint('.')
+dprint ('Do you wish to start the game or not ? ')
 start = input()
 if start in begin:
-    dprint('let us begin')
+    dprint('then let us begin')
     dprint('.')
     time.sleep(1)
     dprint('.')
     time.sleep(1)
     dprint('.')
     time.sleep(1)
-    dprint
+    dprint('You awake from stasis, a flash of blinding light, your memory is fogged and cloudy. "Welcome back Captain, I have some troubling news, we have docked with an unknown outpost, it seems the outpost had a tractor beam which overpowered my engines. I am currently helpless to move the ship, and the outpost is not responding to our communications."')
 else:
     dprint('well I guess you better get on with your incredible existence then, Human..')
     quit()
