5b60e8f583ffb178f1373ebd737f2db2ee6ecab4
caninereason <theciaranneeson@hotmail.com>
2022-11-24 22:42:28 +0000

    cleaned or statement

--- run.py
+++ run.py
@@ -1,8 +1,11 @@
+
+
+
 name = input("What is your name ")
 print(f'Greetings {name}! welcome to the outpost')
 start = input ('Do you wish to start the game or not ')
-if start == 'play||p||y||yes||start||s':
+if start == 'play'or'p'or'y'or'yes'or'start'or's':
     print('let us begin')
 else:
-    print('well I guess you better get on with your amazing existence then')
+    print('well I guess you better get on with your incredible existence then, Human')
     quit()
